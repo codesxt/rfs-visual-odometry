174ee1fd721ae4b421e10352c3c88847558cf36a
Bruno Faúndez Valenzuela <gameover_sxt_nah@hotmail.com>
2015-08-13 09:37:14 -0300

    First commit.

---
+++ README.md
@@ -0,0 +1,2 @@
+#RFS-VISUAL-ODOMETRY
+This is an implementation of RFS-VISUAL-ODOMETRY based on the paper "Single Camera Visual Odometry Based on Random Finite Set Statistics" by Feizhu Zhang, Hauke Stähle, Andre Gaschler, Christian Buckl and Alois Knoll.
---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+/**
+* This file is part of RFS-VISUAL-ODOMETRY.
+*
+* This implementation is based on the paper "Single Camera Visual
+* Odometry Based on Random Finite Set Statistics" by Feizhu Zhang,
+* Hauke Stähle, Andre Gaschler, Christian Buckl and Alois Knoll.
+*
+* RFS-VISUAL-ODOMETRY is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* RFS-VISUAL-ODOMETRY is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with RFS-VISUAL-ODOMETRY. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <iostream>
+#include <opencv2/core/core.hpp>
+
+using namespace std;
+
+int main(int argc, char **argv){
+
+  return 0;
+}
